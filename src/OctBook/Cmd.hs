1bd64e5f6cc7d4d8b516ae5a34bf247942b57a47
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-11-15 21:18:09 +0900

    Feat: invite github org team

--- src/OctBook/Cmd.hs
+++ src/OctBook/Cmd.hs
@@ -6,14 +6,17 @@ module OctBook.Cmd
 import           RIO
 import qualified RIO.List                               as L
 
+import           Data.Fallible
 import           GitHub.Data.Name                       (mkName)
 import qualified GitHub.Endpoints.Organizations.Members as GitHub
+import qualified GitHub.Endpoints.Organizations.Teams   as GitHub
 import qualified Mix.Plugin.GitHub                      as MixGitHub
 import           OctBook.Config                         (User)
 import           OctBook.Env
 
 inviteOrg :: [Text] -> RIO Env ()
-inviteOrg [] = mapM_ inviteUserToGitHubOrg =<< asks (view #config)
+inviteOrg [] =
+  mapM_ inviteUserToGitHubOrg =<< asks (view #config)
 inviteOrg userIDs = do
   users <- asks (view #config)
   forM_ userIDs $ \idx ->
@@ -22,10 +25,14 @@ inviteOrg userIDs = do
       Nothing   -> logWarn (display $ "user not found: " <> idx)
 
 inviteTeam :: [Text] -> [Text] -> RIO Env ()
-inviteTeam _ _ = showNotImpl
-
-showNotImpl :: MonadIO m => m ()
-showNotImpl = hPutBuilder stdout "not yet implement command.\n"
+inviteTeam [] teamIDs =
+  mapM_ (inviteUserToGitHubOrgTeam teamIDs) =<< asks (view #config)
+inviteTeam userIDs teamIDs = do
+  users <- asks (view #config)
+  forM_ userIDs $ \idx ->
+    case L.find (\user -> user ^. #id == idx) users of
+      Just user -> inviteUserToGitHubOrgTeam teamIDs user
+      Nothing   -> logWarn (display $ "user not found: " <> idx)
 
 inviteUserToGitHubOrg :: User -> RIO Env ()
 inviteUserToGitHubOrg user = do
@@ -37,8 +44,36 @@ inviteUserToGitHubOrg user = do
     Left err -> logDebug (displayShow err) >> logWarn (display failure)
     Right _  -> logInfo $ display success
   where
-    failure = "fail invite user: " <> user ^. #id
+    failure = mconcat
+      [ "Fail: invite user: ", user ^. #id,  ", org: ", user ^. #org ]
     success = mconcat
       [ "Success: invite "
       , user ^. #name, "(", user ^. #id, ")", " to ", user ^. #org, "."
+      ]
+
+inviteUserToGitHubOrgTeam :: [Text] -> User -> RIO Env ()
+inviteUserToGitHubOrgTeam teamIDs user =
+  forM_ (L.intersect teamIDs' $ user ^. #teams) $ \teamId -> evalContT $ do
+    resp <- MixGitHub.fetch $ \auth -> GitHub.teamInfoByName' (Just auth)
+      (mkName Proxy $ user ^. #org)
+      (mkName Proxy teamId)
+    team <- resp ??= exit . logFail teamId
+    resp' <- MixGitHub.fetch $ \auth -> GitHub.addTeamMembershipFor' auth
+      (GitHub.teamId team)
+      (mkName Proxy $ user ^. #id)
+      GitHub.RoleMember
+    lift $ case resp' of
+      Left err -> logFail teamId err
+      Right _  -> logInfo (display $ success teamId)
+  where
+    teamIDs' = if null teamIDs then user ^. #teams else teamIDs
+    logFail teamId err = logDebug (displayShow err) >> logWarn (display $ failure teamId)
+    failure teamId = mconcat
+      [ "Fail: invite user: ", user ^. #id
+      ,  ", org: ", user ^. #org, ", team: ", teamId
+      ]
+    success teamId = mconcat
+      [ "Success: invite "
+      , user ^. #name, "(", user ^. #id, ")"
+      , " to ", user ^. #org, ":", teamId, "."
       ]
